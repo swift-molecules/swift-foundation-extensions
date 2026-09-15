8adb131db29ba67b13f0ee5a17ec79ec6fa3a498
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-15 18:49:09 +0200

    WIP

--- Sources/FoundationEssentials Extensions/Swift.Array.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-extension Array {
-    /// Convenience over `IndexSet.move(_:offsets:to:)`.
-    public mutating func move(offsets: IndexSet, to destination: Int) {
-        self = IndexSet.move(self, offsets: offsets, to: destination)
-    }
-}
--- Tests/Foundation Extensions Tests/Foundation Extensions Tests.swift
+++ Tests/Foundation Extensions Tests/Foundation Extensions Tests.swift
@@ -8,9 +8,6 @@ struct `Foundation Extensions` {
 
     @Test
     func `The umbrella re-exports every type module`() {
-        var array = [1, 2]
-        array.move(offsets: [1], to: 0)
-        #expect(array == [2, 1])
         #expect(DateComponents.zero == DateComponents())
         #expect(TimeInterval.minute == 60)
     }
